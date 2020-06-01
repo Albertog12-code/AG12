f2a10d3c704875d25453c240e6ed4001bbfb79c8
Albertog12-code <66281310+Albertog12-code@users.noreply.github.com>
2020-06-01 16:07:20 -0700

    Update Practice.C

--- Practice.C
+++ Practice.C
@@ -1,1 +1,15 @@
 
+#include <cS50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  char c = get_char( "Is Mario feeling ok?\n");
+  if (C == 'Y')
+  { 
+    printf("Mario is feeling comfortable.\n");
+  }
+  else if (c == 'N')
+  {
+    printf("Mario is not feeling comfortable.\n");
+  }
